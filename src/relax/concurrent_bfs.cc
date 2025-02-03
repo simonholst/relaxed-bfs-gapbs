2b03a43cceb75842b1173f34935a4bccd9c921c6
Johan Selin <johanselin99@gmail.com>
2025-02-03 13:15:41 +0100

    Concurrent working sequentially

--- src/relax/concurrent_bfs.cc
+++ src/relax/concurrent_bfs.cc
@@ -10,11 +10,11 @@
 #include "../platform_atomics.h"
 #include "../pvector.h"
 #include "bfs_helper.h"
+#include <boost/lockfree/queue.hpp>
 
 __thread int thread_id;
 
-pvector<NodeID> InitParent(const Graph &g)
-{
+pvector<NodeID> InitParent(const Graph &g) {
     pvector<NodeID> parent(g.num_nodes());
     #pragma omp parallel for
     for (NodeID n = 0; n < g.num_nodes(); n++)
@@ -22,36 +22,65 @@ pvector<NodeID> InitParent(const Graph &g)
     return parent;
 }
 
+pvector<NodeID> InitDepth(const Graph &g) {
+    pvector<int32_t> depth(g.num_nodes());
+    #pragma omp parallel for
+    for (int64_t n = 0; n < g.num_nodes(); n++)
+        depth[n] = 0;
+    return depth;
+}
+
 pvector<NodeID> ConcurrentBFS(const Graph &g, NodeID source, bool logging_enabled = false)
 {
     pvector<NodeID> parent = InitParent(g);
-    queue<NodeID> queue;
+    pvector<int32_t> depth = InitDepth(g);
+    boost::lockfree::queue<NodeID> queue(false);
     queue.push(source);
     parent[source] = source;
-    while (!queue.empty())
-    {
-        NodeID node = queue.front();
-        queue.pop();
-        g.out_neigh(node);
-        for (NodeID v : g.out_neigh(node))
-        {
-            NodeID curr_val = parent[v];
-            if (curr_val < 0)
-            {
-                parent[v] = node;
-                queue.push(v);
+	while (true)
+	{
+        NodeID node;
+		while (queue.pop(node))
+		{
+            g.out_neigh(node);
+            // uint64_t *neighbors;
+			// uint64_t size = get_neighbors(g, current, &neighbors);
+			// uint64_t current_distance = g->distances[current];
+
+            int32_t parent_depth = depth[node];
+            int32_t new_depth = parent_depth + 1; 
+
+            for (NodeID v : g.out_neigh(node)) {
+                int32_t curr_parent = parent[v];
+                if (curr_parent < 0) {
+                    if (compare_and_swap(parent[v], curr_parent, node)) {
+                        queue.push(v);
+                    }
+                    else {
+                        int32_t current_depth = depth[v];
+                        if (current_depth > new_depth) {
+                            // assign new parent and depth
+                        }
+                    }
+                }
+                else {
+                    int32_t current_depth = depth[v];
+                    if (current_depth > new_depth) {
+                        // assign new parent and depth
+                    }
+                    queue.push(v);
+                }
             }
-        }
-    }
-    return parent;
+
+		}
+        return parent;
+	}
 }
 
-int main(int argc, char *argv[])
-{
-    CLApp cli(argc, argv, "relaxed breadth-first search");
+int main(int argc, char *argv[]) {
+    CLApp cli(argc, argv, "concurrent breadth-first search");
 
-    if (!cli.ParseArgs())
-    {
+    if (!cli.ParseArgs()) {
         printf("Exiting");
         return -1;
     }
