8bfee90c9c3bbcc61805a1a256e0ac8876c64482
Simon Holst <simonhol@student.chalmers.se>
2025-03-21 15:31:39 +0100

    Updated CAS for nodes to avoid compiler warning

--- src/relax/node.h
+++ src/relax/node.h
@@ -14,8 +14,12 @@ struct Node
 // Function to perform CAS on a 64-bit struct
 bool compare_and_swap(Node &x, Node old_val, Node new_val)
 {
+    static_assert(sizeof(Node) == sizeof(uint64_t), "Node size must be 8 bytes");
+
+    uint64_t old_val_int, new_val_int;
+    std::memcpy(&old_val_int, &old_val, sizeof(uint64_t));
+    std::memcpy(&new_val_int, &new_val, sizeof(uint64_t));
+
     uint64_t *x_ptr = reinterpret_cast<uint64_t *>(&x);
-    uint64_t old_val_int = *reinterpret_cast<uint64_t *>(&old_val);
-    uint64_t new_val_int = *reinterpret_cast<uint64_t *>(&new_val);
     return __sync_bool_compare_and_swap(x_ptr, old_val_int, new_val_int);
 }
