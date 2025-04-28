33e837c52574f2948f047db12cbcc6ae507c1466
Johan Selin <johanselin99@gmail.com>
2025-04-28 16:16:25 +0200

    Modify script for ithaca

--- athena-bench.sh
+++ athena-bench.sh
@@ -16,7 +16,7 @@ get_progress
 
 if [ $progress -eq 0 ]; then
     echo "Running DO_TD road_usa benchmarks..."
-    python3 bench.py -args "-g graphs/road_usa.mtx.sg -n 32"          -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/usa -a DO_TD -p athena_ht
+    python3 bench.py -args "-g graphs/road_usa.mtx.sg -n 2"          -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/usa -a DO_TD -p ithaca_ht
     echo 1 > $PROGRESS_FILE
 else
     echo "Skipping DO_TD road_usa benchmarks, already completed."
@@ -26,7 +26,7 @@ get_progress
 
 if [ $progress -eq 1 ]; then
     echo "Running DO_TD road-europe benchmarks..."
-    python3 bench.py -args "-g graphs/europe_osm.mtx.sg -n 32"           -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/europe -a DO_TD -p athena_ht
+    python3 bench.py -args "-g graphs/europe_osm.mtx.sg -n 2"           -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/europe -a DO_TD -p ithaca_ht
     echo 2 > $PROGRESS_FILE
 else
     echo "Skipping DO_TD road-europe benchmarks, already completed."
@@ -36,7 +36,7 @@ get_progress
 
 if [ $progress -eq 2 ]; then
     echo "Running DO_TD road-asia benchmarks..."
-    python3 bench.py -args "-g graphs/asia_osm.mtx.sg -n 32"          -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/asia -a DO_TD -p athena_ht
+    python3 bench.py -args "-g graphs/asia_osm.mtx.sg -n 2"          -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/asia -a DO_TD -p ithaca_ht
     echo 3 > $PROGRESS_FILE
 else
     echo "Skipping DO_TD road-asia benchmarks, already completed."
@@ -46,7 +46,7 @@ get_progress
 
 if [ $progress -eq 3 ]; then
     echo "Running DO_TD hugebubbles-00020 benchmarks..."
-    python3 bench.py -args "-g graphs/hugebubbles-00020.mtx.sg -n 32" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/hugebubbles -a DO_TD -p athena_ht
+    python3 bench.py -args "-g graphs/hugebubbles-00020.mtx.sg -n 2" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/DO_TD/hugebubbles -a DO_TD -p ithaca_ht
     echo 4 > $PROGRESS_FILE
 else
     echo "Skipping DO_TD hugebubbles-00020 benchmarks, already completed."
@@ -61,7 +61,7 @@ get_progress
 
 if [ $progress -eq 4 ]; then
     echo "Running kronecker benchmarks - DO"
-    python3 bench.py -args "-g 24 -n 32" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/kronecker -a DO -p athena_ht
+    python3 bench.py -args "-g 24 -n 2" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/kronecker -a DO -p ithaca_ht
     echo 5 > $PROGRESS_FILE
 else
     echo "Skipping kronecker DO benchmarks, already completed."
@@ -69,7 +69,7 @@ fi
 
 if [ $progress -eq 5 ]; then
     echo "Running kronecker benchmarks - DO_TD"
-    python3 bench.py -args "-g 24 -n 32" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/kronecker -a DO_TD -p athena_ht
+    python3 bench.py -args "-g 24 -n 2" -t 1 2 4 8 16 32 64 128 256 512 -d no -o athena/kronecker -a DO_TD -p ithaca_ht
     echo 6 > $PROGRESS_FILE
 else
     echo "Skipping kronecker DO_TD benchmarks, already completed."
@@ -82,7 +82,7 @@ for algorithm in "DCBO_FAA_BOD" "DCBO_FAA_DAD" "DCBO_FAA_BATCHING" "DCBO_FAA"; d
     if [ $progress -eq $loop_progress_count ]; then
         echo "Running kronecker benchmarks - $algorithm"
         echo $(($progress + 1)) > $PROGRESS_FILE
-        python3 bench.py -args "-g 24 -n 32" -t 1 32 64 128 256 512 -d both -bs 16 32 64 -sq 64 128 -o athena/kronecker -a $algorithm -p athena_ht
+        python3 bench.py -args "-g 24 -n 2" -t 1 32 64 128 256 512 -d both -bs 16 32 64 -sq 64 128 -o athena/kronecker -a $algorithm -p ithaca_ht
     else
         echo "Skipping kronecker $algorithm benchmarks, already completed."
     fi
@@ -100,7 +100,7 @@ get_progress
 
 if [ $progress -eq 10 ]; then
     echo "Running FAA road_usa benchmarks..."
-    python3 bench.py -args "-g graphs/road_usa.mtx.sg -n 32"          -t 1 32 64 128 256 512 -d both -o athena/FAA/usa -a FAA -p athena_ht
+    python3 bench.py -args "-g graphs/road_usa.mtx.sg -n 2"          -t 1 32 64 128 256 512 -d both -o athena/FAA/usa -a FAA -p ithaca_ht
     echo 11 > $PROGRESS_FILE
 else
     echo "Skipping FAA road_usa benchmarks, already completed."
@@ -110,7 +110,7 @@ get_progress
 
 if [ $progress -eq 11 ]; then
     echo "Running FAA road-europe benchmarks..."
-    python3 bench.py -args "-g graphs/europe_osm.mtx.sg -n 32"           -t 1 32 64 128 256 512 -d both -o athena/FAA/europe -a FAA -p athena_ht
+    python3 bench.py -args "-g graphs/europe_osm.mtx.sg -n 2"           -t 1 32 64 128 256 512 -d both -o athena/FAA/europe -a FAA -p ithaca_ht
     echo 12 > $PROGRESS_FILE
 else
     echo "Skipping FAA road-europe benchmarks, already completed."
@@ -120,7 +120,7 @@ get_progress
 
 if [ $progress -eq 12 ]; then
     echo "Running FAA road-asia benchmarks..."
-    python3 bench.py -args "-g graphs/asia_osm.mtx.sg -n 32"          -t 1 32 64 128 256 512 -d both -o athena/FAA/asia -a FAA -p athena_ht
+    python3 bench.py -args "-g graphs/asia_osm.mtx.sg -n 2"          -t 1 32 64 128 256 512 -d both -o athena/FAA/asia -a FAA -p ithaca_ht
     echo 13 > $PROGRESS_FILE
 else
     echo "Skipping FAA road-asia benchmarks, already completed."
@@ -130,7 +130,7 @@ get_progress
 
 if [ $progress -eq 13 ]; then
     echo "Running FAA hugebubbles-00020 benchmarks..."
-    python3 bench.py -args "-g graphs/hugebubbles-00020.mtx.sg -n 32" -t 1 32 64 128 256 512 -d both -o athena/FAA/hugebubbles -a FAA -p athena_ht
+    python3 bench.py -args "-g graphs/hugebubbles-00020.mtx.sg -n 2" -t 1 32 64 128 256 512 -d both -o athena/FAA/hugebubbles -a FAA -p ithaca_ht
     echo 14 > $PROGRESS_FILE
 else
     echo "Skipping FAA hugebubbles-00020 benchmarks, already completed."
@@ -140,7 +140,7 @@ get_progress
 
 if [ $progress -eq 14 ]; then
     echo "Running kronecker benchmarks - FAA"
-    python3 bench.py -args "-g 24 -n 32" -t 1 32 64 128 256 512 -d both -o athena/kronecker -a FAA -p athena_ht
+    python3 bench.py -args "-g 24 -n 2" -t 1 32 64 128 256 512 -d both -o athena/kronecker -a FAA -p ithaca_ht
     echo 15 > $PROGRESS_FILE
 else
     echo "Skipping kronecker FAA benchmarks, already completed."
